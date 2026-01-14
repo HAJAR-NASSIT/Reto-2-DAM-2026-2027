6bd6b0f2021b3ec5aba20cfa2bd3c30697bde84f
HAJAR-NASSIT <hajar.nassit@elorrieta-errekamari.com>
2026-01-14 16:57:34 +0100

    añadir una prueba para ver si fonciona la conneccion entre database y la app

--- src/main/java/VentaEntradasCenima/Modelo/BBDD/Utils/DBUtils.java
+++ src/main/java/VentaEntradasCenima/Modelo/BBDD/Utils/DBUtils.java
@@ -11,28 +11,49 @@ public class DBUtils {
 	private static String rutaRelativa = "src\\main\\resources\\ConexionDB.txt";
 	private static boolean config = false;
 
+	/**
+	 * 
+	 * @return
+	 */
 	public static Connection getConnection() {
+		// Variables
+		Connection conexion;
+		
+		// Leemos las variables de conexión desde un fichero de configuración
 		if (!config) {
-			leerFichero();
+			try {
+				leerFichero();
+			} catch (IOException error_de_IO) {
+				// TODO Auto-generated catch block
+				System.out.println("Error al leer el fichero inicial.");
+				error_de_IO.printStackTrace();
+			}
 			config = true;
 		}
+		
 		try {
-			Connection conexion = DriverManager.getConnection(url, user, password);
-			return conexion;
+			conexion = DriverManager.getConnection(url, user, password);
 
 		} catch (SQLException e) {
 			System.out.println("Fallo en la conecion:" + e.getMessage());
-			return null;
+			conexion = null;
 		}
+		
+		return conexion;
 	}
 
-	private static void leerFichero() {
+	private static void leerFichero() throws IOException {
 		File ficheroDB = new File(rutaRelativa);
+		BufferedReader br = null;
+		
 		if (!ficheroDB.exists()) {
 			System.out.println("No se encontre el archivo de configuracion : " +rutaRelativa);
 
 		}
-		try (BufferedReader br = new BufferedReader(new FileReader(ficheroDB))) {
+		
+		try{
+			br = new BufferedReader(new FileReader(ficheroDB));
+			
 			String linea;
 
 			while ((linea = br.readLine()) != null) {
@@ -48,9 +69,12 @@ public class DBUtils {
 					password = linea.substring(9);
 				}
 			}
-			br.close();
+			
 		} catch (IOException e) {
 			System.out.println("Error al leer el fichero de base de datos ." + e.getMessage());
+		}
+		finally {
+			br.close();
 		}
 	}
 
@@ -63,19 +87,36 @@ public class DBUtils {
 			System.out.println("Erreur al cerrar la conexion : " + e.getMessage());
 		}
 	}
+	
 	 // Méthode de test
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws SQLException {
+		Connection con = null;
 		
-		 try (Connection con = getConnection()) {
-	            if (con != null) {
-	                System.out.println("Conexión OK!");
-	            } else {
-	                System.out.println("Error de conexión!");
-	            }
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
+		try{
+			con = getConnection();
+			 
+            if (con != null) {
+                System.out.println("Conexión OK!");
+            } else {
+                System.out.println("Error de conexión!");
+            }
+        } catch (Exception e) {
+        	System.out.println("Fallo en la conexión");
+            e.printStackTrace();
+        }
+		
+		String sql = "Select * from cliente";
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(sql);
+		
+
+while (rs.next()) {
+    // Procesar cada fila
+    String DNI = rs.getString("DNI");
+    String nombre = rs.getString("nombre");
+  
+    System.out.println("ID: " + DNI + ", Nombre: " + nombre);
+}
 	}
 
 }
