67e44c1ef30e0831ada74f302840a6dfff1f08e0
Mohammed <mohammed.aouragh@elorrieta-errekamari.com>
2026-01-22 16:42:38 +0100

    clase pelicula + peliculaDAO

--- src/main/java/VentaEntradasCenima/Modelo/BBDD/GestorDAO/PeliculaDAO.java
+++ src/main/java/VentaEntradasCenima/Modelo/BBDD/GestorDAO/PeliculaDAO.java
@@ -1,5 +1,7 @@
 package VentaEntradasCenima.Modelo.BBDD.GestorDAO;
 
+
+
 import java.sql.*;
 import java.util.ArrayList;
 
@@ -9,40 +11,52 @@ import VentaEntradasCenima.Modelo.Pojos.Pelicula;
 
 public class PeliculaDAO {
 
-	public ArrayList <Pelicula> listaPeliculasPorSecionMasCerca(){
-		ArrayList <Pelicula> peliculas = new ArrayList<>();
+	public  ArrayList <Pelicula> listaPeliculasPorSecionMasCerca(){
+		 ArrayList <Pelicula> peliculas = new ArrayList<>();
 		Connection connection = null;
-		PreparedStatement preparedStatement = null; 
-		ResultSet resultSet = null;
+		PreparedStatement preparedStatement = null ; 
+		ResultSet resultSet =null;
 
 		try {
 			connection = DBUtils.getConnection() ;
-			preparedStatement = connection.prepareStatement(SQLQuerys.SELECT_PELICULAS_ORDENADAS_POR_SESION_MAS_CERCANA);
+			preparedStatement = connection.prepareStatement( SQLQuerys.SELECT_PELICULAS_ORDENADAS_POR_SESION_MAS_CERCANA);
 			
 			resultSet = preparedStatement.executeQuery();
 			
-			while (resultSet.next()) {
+			while(resultSet.next() ) {
 				
 				Pelicula pelicula = new Pelicula();
 		
-				pelicula.setIdPelicula(resultSet.getInt("id_pelicula"));
-				pelicula.setClasificacion(resultSet.getString("nombre_genero"));
+				pelicula.setIdPelicula(resultSet.getInt("id_pelicula"))   ;
+				pelicula.setNombreGenero(resultSet.getString("nombre_genero"));
 				pelicula.setTitulo(resultSet.getString("titulo")) ;
-				pelicula.setDuracion(resultSet.getInt("duracion")); 
+				pelicula.setDuracion(resultSet.getInt("duracion")) ; 
 				pelicula.setIdioma(resultSet.getString("idioma"));
 				pelicula.setDescripcion(resultSet.getString("descripcion"));
+				pelicula.setImagen(resultSet.getString("imagen")) ;
 				
-				peliculas.add(pelicula);
+				peliculas.add(pelicula) ;
 			}
 			
 		} catch (SQLException e) {
 			System.out.println("Error al listar Peliculas : " + e.getMessage());
 		} finally {
-			DBUtils.cerrarConexion(connection);
+			try{
+                if (resultSet != null) {
+                	resultSet.close();
+                }
+                if (preparedStatement != null) {
+                	preparedStatement.close();
+                }
+            }catch (SQLException e) {
+            	
+            }
+            DBUtils.cerrarConexion(connection);
 		}
 		
-		return peliculas;
-	
+	return peliculas;
 	}
+	
+	
 	
 }
--- src/main/java/VentaEntradasCenima/Modelo/Pojos/Pelicula.java
+++ src/main/java/VentaEntradasCenima/Modelo/Pojos/Pelicula.java
@@ -3,169 +3,93 @@ package VentaEntradasCenima.Modelo.Pojos;
 
 public class Pelicula {
 
-<<<<<<< HEAD
 	private int idPelicula;
 	private String nombreGenero;
-=======
-	private int idpelicula;
->>>>>>> 94b37301267835b884a330ad4b0ab3d004c2ed5e
 	private String titulo;
 	private int duracion;
 	private String idioma;
 	private String descripcion;
-	
-<<<<<<< HEAD
+	private String imagen;
+
 	public Pelicula() {
-		
-		
+	
 	}
+
+	public Pelicula(int idPelicula, String nombreGenero, String titulo, int duracion, String idioma, String descripcion,
+			String imagen) {
 	
-	
-	
-	
-	public Pelicula(int idPelicula, String nombreGenero, String titulo, int duracion, String idioma,
-			String descripcion) {
 		this.idPelicula = idPelicula;
 		this.nombreGenero = nombreGenero;
-=======
-
-	public Pelicula() {
-	
-	}
-
-
-	public Pelicula(int idPelicula, String titulo, int duracion, String clasificacion, String idioma,
-			String descripcion) {
-		this.idpelicula = idPelicula;
->>>>>>> 94b37301267835b884a330ad4b0ab3d004c2ed5e
 		this.titulo = titulo;
 		this.duracion = duracion;
 		this.idioma = idioma;
 		this.descripcion = descripcion;
+		this.imagen = imagen;
 	}
-
-<<<<<<< HEAD
-
-
 
 	public int getIdPelicula() {
 		return idPelicula;
 	}
+
 	public void setIdPelicula(int idPelicula) {
 		this.idPelicula = idPelicula;
 	}
+
 	public String getNombreGenero() {
 		return nombreGenero;
 	}
+
 	public void setNombreGenero(String nombreGenero) {
 		this.nombreGenero = nombreGenero;
 	}
-	public String getTitulo() {
-		return titulo;
-	}
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	public int getDuracion() {
-		return duracion;
-	}
-	public void setDuracion(int duracion) {
-		this.duracion = duracion;
-	}
-	public String getIdioma() {
-		return idioma;
-	}
-	public void setIdioma(String idioma) {
-		this.idioma = idioma;
-	}
-	public String getDescripcion() {
-		return descripcion;
-	}
-=======
-
-	public int getIdPelicula() {
-		return idpelicula;
-	}
-
-
-	public void setIdPelicula(int idPelicula) {
-		this.idpelicula = idPelicula;
-	}
-
 
 	public String getTitulo() {
 		return titulo;
 	}
 
-
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
-
 
 	public int getDuracion() {
 		return duracion;
 	}
 
-
 	public void setDuracion(int duracion) {
 		this.duracion = duracion;
 	}
-
-
-	public String getClasificacion() {
-		return clasificacion;
-	}
-
-
-	public void setClasificacion(String clasificacion) {
-		this.clasificacion = clasificacion;
-	}
-
 
 	public String getIdioma() {
 		return idioma;
 	}
 
-
 	public void setIdioma(String idioma) {
 		this.idioma = idioma;
 	}
-
 
 	public String getDescripcion() {
 		return descripcion;
 	}
 
-
->>>>>>> 94b37301267835b884a330ad4b0ab3d004c2ed5e
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
 
+	public String getImagen() {
+		return imagen;
+	}
 
-<<<<<<< HEAD
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
 
 	@Override
 	public String toString() {
 		return "Pelicula [idPelicula=" + idPelicula + ", nombreGenero=" + nombreGenero + ", titulo=" + titulo
-				+ ", duracion=" + duracion + ", idioma=" + idioma + ", descripcion=" + descripcion + "]";
-=======
-	@Override
-	public String toString() {
-		return "Pelicula [idPelicula=" + idpelicula + ", titulo=" + titulo + ", duracion=" + duracion
-				+ ", clasificacion=" + clasificacion + ", idioma=" + idioma + ", descripcion=" + descripcion + "]";
->>>>>>> 94b37301267835b884a330ad4b0ab3d004c2ed5e
+				+ ", duracion=" + duracion + ", idioma=" + idioma + ", descripcion=" + descripcion + ", imagen="
+				+ imagen + "]";
 	}
 
 	
-<<<<<<< HEAD
 	
-	
-}
-    
-	
-
-=======
 	}
->>>>>>> 94b37301267835b884a330ad4b0ab3d004c2ed5e
