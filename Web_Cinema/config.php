f132558bfbbd0ef154ca282b9ee02fd19d8c48da
HAJAR-NASSIT <hajar.nassit@elorrieta-errekamari.com>
2026-02-04 16:12:07 +0100

    verificacion de la pagina en general

--- Web_Cinema/config.php
+++ Web_Cinema/config.php
@@ -1,12 +1,13 @@
+//http://10.5.6.70/Web_Cinema/index.php
 <?php
 // config.php - SOLO si no hay sesión iniciada
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-$servername = "127.0.0.1:3307";
-$username = "root";
-$password = "Hsn19611";
+$servername = "10.5.6.68:33060";
+$username = "dam";
+$password = "Elorrieta9753$";
 $dbname = "cine_elorrieta";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
